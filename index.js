c9ca00a4e20fbfa67f1dffa9f8036550a8a503ae
gecko_wu <gecko_wu@163.com>
2019-03-30 23:25:55 +0800

    修改图片和新闻详情

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 app.use(express.static('resource'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*'); //访问控制允许来源：所有
@@ -33,16 +36,61 @@ app.get('/api/getNewsList', function (req, res) {
   res.send(newsList);
 });
 
+app.get('/api/getNewsDetail', function (req, res) {
+  const newsDetail = path.resolve(__dirname, 'resource/json/news.json');
+  fs.readFile(newsDetail, 'utf-8', function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(JSON.parse(data).data[0]);
+    }
+  })
+});
+
+app.post('/api/comment', function (req, res) {
+  const file = path.resolve(__dirname, 'resource/json/news.json');
+  const review = req.body;
+  fs.readFile(file, 'utf-8', function (err, data) {
+    if (err) {
+      console.log(err)
+    } else {
+      const news = JSON.parse(data);
+      news.data[0].review.unshift(review);
+      fs.writeFile(file, JSON.stringify(news), function (err2) {
+        if (err2) {
+          console.log(err2)
+        } else {
+          res.send('ok');
+        }
+      })
+    }
+  })
+});
+
 app.get('/api/getCarPhotos', function(req, res) {
   const carJsonFile = path.resolve(__dirname, 'resource/json/carPhoto.json');
+  const labelId = req.query.id;
   fs.readFile(carJsonFile, 'utf-8', function (err, data) {
+    const photos = [];
     if (err) {
       res.send([]);
     } else {
-      res.send(data);
+      const allData = JSON.parse(data);
+      if (labelId === '0') {
+        res.send(allData.data);
+      } else {
+        res.send(allData.data.filter((p) => {
+          if (labelId === '5') {
+            return parseInt(p.id) % 5 === 0
+          } else {
+            return parseInt(p.id) % 5 === parseInt(labelId)
+          }
+        })
+        );
+      }
     }
   })
-})
+});
 
 const server = app.listen(8081, function () {
 
